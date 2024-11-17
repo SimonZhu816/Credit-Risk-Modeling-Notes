b89d23b38f3daa9e4b7dea1def5da54f044a3e22
dayanzaifei <62919538+SimonZhu816@users.noreply.github.com>
2024-11-17 17:01:48 +0800

    Update dnn.py

--- dnn.py
+++ dnn.py
@@ -48,4 +48,84 @@ def tfrecords_reader_dataset(filename,repeat=True,n_readers=2,batch_size=1024,n_
   dataset = dataset.batch(batch_size)
   return dataset
 
+batch_size = 1024
+tfrecords_train = tfrecords_reader_dataset("train.tfrecords",batch_size=batch_size)
+tfrecords_val = tfrecords_reader_dataset("testA.tfrecords",batch_size=batch_size)
+
+gc.collect()
+class AutoInt(Layer):
+  def __init__(self,dim=16,n_head=2,**kwargs):
+    self.ndims = dim
+    self.n_head = n_head
+    super(AutoInt,self).__int__(**kwargs)
+  def build(self,input_shape):
+    assert len(input_shape) ==3, "Input embedding feature map must be 3_D tensor."
+    input_dim = input_shape[-1]
+    self.W_Q = []
+    self.W_K = []
+    self.W_V = []
+    # 构建多个attention
+    for i in range(self.n_head):
+      # 初始化W_Q、W_K、W_V
+      self.W_Q.append(K.variable(tf.random.trancated_normal(shape=(input_dim,self.ndims)), name = f"AI_query_{self.name}_"+str(i)))
+      self.W_K.append(K.variable(tf.random.trancated_normal(shape=(input_dim,self.ndims)), name = f"AI_key_{self.name}_"+str(i)))
+      self.W_V.append(K.variable(tf.random.trancated_normal(shape=(input_dim,self.ndims)), name = f"AI_value_{self.name}_"+str(i)))
+    self.w_res = K.variable(tf.random.trancated_normal(shape=(input_dim,self.ndims*self.n_head)), name = f"AI_w_res_{self.name}_"+str(i))
+    super(AutoInt,self).build(input_shape) # 一定要在最后调用它
+  def call(self,x):
+    attention_heads = []
+    for i in range(self.n_head):
+      # 映射到d维空间
+      embed_q = tf.mutul(x,self.W_Q[i])
+      embed_k = tf.mutul(x,self.W_K[i])
+      embed_v = tf.mutul(x,self.W_V[i])
+      # 计算attention
+      energy = tf.mutul(embed_q,tf.transpose(embed_k,[0,2,1]))
+      attention = tf.nn.softmax(energy)
+      attention_output = tf.mutul(attention,embed_v)
+      attention_heads.append(attention_output)
+    # concat multi head
+    multi_attention_output = K.concatenate(attention_heads,axis=-1)
+    # ResNet
+    output = multi_attention_output + tf.matmul(x,self.w_res)
+    return K.relu(output)
+  def compute_output_shape(self,input_shape):
+    output_shape = list(input_shape)
+    output_shape[-1] = self.ndims * self.n_head
+    return (output_shape)
+  def get_config(self):
+    config = {'ndims':self.ndims, 'n_head':self.n_head}
+    base_config = super(AutoInt,self).get_config()
+    base_config.update(config)
+    return base_config
+
+class NoneLayer(Layer):
+  def __init__(self,**kwargs):
+    super(NoneLayer,self).__int__(**kwargs)
+  def build(self,input_shape):
+    # 为该层创建一个可训练的权重
+    self.kernel = self.add_weight(name = 'kernel',
+                                  shape = (1,input_shape[1]),
+                                  initializer = 'uniform',
+                                  trainable = True)
+    super(NoneLayer,self).__int__(**kwargs)
+  def call(self,x):
+    return tf.math.multiply(x,self.kernel)
+  def compute_output_shape(self,input_shape):
+    return (input_shape[0])
+class WeightLayer(Layer):
+  def __init__(self,**kwargs):
+    super(WeightLayer,self).__int__(**kwargs)
+  def build(self,input_shape):
+    # 为该层创建一个可训练的权重
+    self.kernel = self.add_weight(name = 'kernel',
+                                  shape = (input_shape[-2],16),
+                                  initializer = 'uniform',
+                                  trainable = True)
+    super(WeightLayer,self).__int__(**kwargs)
+
+  def call(self,x):
+    return tf.math.multiply(x,self.kernel)
+  def compute_output_shape(self,input_shape):
+    return (input_shape[0])
 # S3：评估单元
